77496ede6c22b9adb38119cc2722bdc2cafad78a
xappyyy <surin123.james@gmail.com>
2023-12-29 11:39:42 +0700

    refactor: more informations on response

--- src/utils/gin_response.go
+++ src/utils/gin_response.go
@@ -4,12 +4,17 @@ import "github.com/gin-gonic/gin"
 
 func HandleErrorResponse(ctx *gin.Context, statusCode int, errorMessage string) {
 	ctx.AbortWithStatusJSON(statusCode, gin.H{
-		"error": errorMessage,
+		"success": false,
+		"error": gin.H{
+			"code": statusCode,
+			"message": errorMessage,
+		},
 	})
 }
 
 func RespondWithData(ctx *gin.Context, statusCode int, data gin.H) {
 	ctx.JSON(statusCode, gin.H{
+		"success": true,
 		"data": data,
 	})
 }
