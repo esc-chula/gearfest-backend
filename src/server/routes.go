34327ed8231eba79654108cf56fcec0ba0eb5877
xappyyy <surin123.james@gmail.com>
2024-01-02 15:05:33 +0700

    feat: cors

--- src/server/routes.go
+++ src/server/routes.go
@@ -4,12 +4,18 @@ import (
 	"github.com/esc-chula/gearfest-backend/src/config"
 	"github.com/esc-chula/gearfest-backend/src/controllers"
 	"github.com/esc-chula/gearfest-backend/src/server/repositories"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func loadRoutes(db *gorm.DB, cfg config.GoogleConfig) *gin.Engine {
 	g := gin.New()
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowHeaders = append(corsConfig.AllowHeaders, "Authorization")
+	corsConfig.AllowCredentials = true
+	corsConfig.AllowAllOrigins = true
+	g.Use(cors.New(corsConfig))
 	userRoutes := g.Group("/user")
 	userRoutes.Use(Validation(cfg))
 	loadUserRoutes(userRoutes, db)
